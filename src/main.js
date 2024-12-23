4f3f22c759dd3ac347ca0c03857274ff441c4f5a
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-23 13:04:23 +0900

    add: gitignore

--- src/main.js
+++ src/main.js
@@ -1,19 +1,24 @@
+function example(param) {
+    console.log(typeof param);
+}
+example("hello");
+example(2024);
+example(true);
+example(undefined);
 /**
  * @intersection & AND
  * @union | OR
  */
-function printSome(param) {
-    if ("value" in param)
-        console.log(param.value);
-    if ("data" in param)
-        console.log(param.data);
-}
-printSome({ value: 2024 });
-printSome({ data: "hello" });
-function printAnother(param) {
-    console.log(param);
-}
-printAnother({
-    value: 2024,
-    data: "hello"
-});
+// function printSome(param: INumber | IString){
+//     if("value" in param) console.log(param.value);
+//     if("data" in param) console.log(param.data);
+// }
+// printSome({value: 2024});
+// printSome({data: "hello"});
+// function printAnother (param: INumber & IString){
+//     console.log(param);
+// }
+// printAnother({
+//     value: 2024,
+//     data: "hello"
+// });
