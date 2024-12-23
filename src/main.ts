514ceedeb06896ba8f56dde0d36169338bb8d1b3
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-23 13:06:19 +0900

    study interface

--- src/main.ts
+++ src/main.ts
@@ -6,38 +6,23 @@ interface IString {
     data: string;
 }
 
-interface IBoolean {
-    data: boolean;
-}
-
-function example(param: any){
-    console.log(typeof param);
-}
-example("hello");
-example(2024);
-example(true);
-example(undefined);
-
-
-
-
 /**
  * @intersection & AND
  * @union | OR
  */
-// function printSome(param: INumber | IString){
-//     if("value" in param) console.log(param.value);
-//     if("data" in param) console.log(param.data);
-// }
-// printSome({value: 2024});
-// printSome({data: "hello"});
+function printSome(param: INumber | IString){
+    if("value" in param) console.log(param.value);
+    if("data" in param) console.log(param.data);
+}
+printSome({value: 2024});
+printSome({data: "hello"});
 
-// function printAnother (param: INumber & IString){
-//     console.log(param);
-// }
-// printAnother({
-//     value: 2024,
-//     data: "hello"
-// });
+function printAnother (param: INumber & IString){
+    console.log(param);
+}
+printAnother({
+    value: 2024,
+    data: "hello"
+});
 
 
