e9e3afb8089ddeb7a1e9eab1087008f3bf10053e
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-26 17:23:10 +0900

    3-10 study decorator

--- src/main.js
+++ src/main.js
@@ -1,51 +1,87 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Book = /** @class */ (function () {
-    function Book(title) {
-        this.title = title;
+// 데코레이터는 함수다 ( 데코레이터 == 함수 )
+// 1. 런타임에서 바로 실행 => 설정값, 등록 ... 쓰일 수 있다
+// 2. 클래스랑 무조건 같이쓴다. 클래스 자체, 메서드, 필드, 파라미드드
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
     }
-    Book.prototype.printTitle = function () {
-        console.log(this.title);
-    };
-    Book.prototype.somethingImpact = function () {
-        this.title = "new book!";
-    };
-    return Book;
-}());
-var Test = /** @class */ (function (_super) {
-    __extends(Test, _super);
-    function Test(title) {
-        return _super.call(this, title) || this;
+    return useValue ? value : void 0;
+};
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
     }
-    Test.prototype.printTitleFromChild = function () {
-        console.log(this.title);
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var Book = function () {
+    var _a;
+    var _instanceExtraInitializers = [];
+    var _greet_decorators;
+    return _a = /** @class */ (function () {
+            function Book(title) {
+                this.title = __runInitializers(this, _instanceExtraInitializers);
+                this.title = title;
+            }
+            Book.prototype.greet = function (param) {
+                var _b;
+                console.log((_b = this === null || this === void 0 ? void 0 : this.title) !== null && _b !== void 0 ? _b : "typescript" + " " + param);
+            };
+            Book.prototype.greet2 = function (param) {
+                console.log(this.title + "2" + param);
+            };
+            return Book;
+        }()),
+        (function () {
+            var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
+            _greet_decorators = [Logging];
+            __esDecorate(_a, null, _greet_decorators, { kind: "method", name: "greet", static: false, private: false, access: { has: function (obj) { return "greet" in obj; }, get: function (obj) { return obj.greet; } }, metadata: _metadata }, null, _instanceExtraInitializers);
+            if (_metadata) Object.defineProperty(_a, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
+        })(),
+        _a;
+}();
+// const book = new Book("abc")
+// book.greet2("이주철")
+function Logging(originalMethod, context) {
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        console.log("something changed");
+        return originalMethod.call.apply(originalMethod, __spreadArray([this], args, false));
     };
-    return Test;
-}(Book));
-// private readonly 예시
-// class Book{
-//     constructor (
-//         private readonly title: string,
-//         private readonly a: any,
-//         private readonly b: any
-//     ) {}
-// }
-// const b: Book = new Book("inflearn", "hello", "world");
-// //b.title = "hello";
-// console.log(b);
+}
+function Start(originalMethod, context) { }
 var b = new Book("inflearn");
-//b.title = "hello";
-console.log(b.printTitle());
+b.greet("hello world");
--- src/main.ts
+++ src/main.ts
@@ -1,48 +1,47 @@
+// 데코레이터는 함수다 ( 데코레이터 == 함수 )
+// 1. 런타임에서 바로 실행 => 설정값, 등록 ... 쓰일 수 있다
+// 2. 클래스랑 무조건 같이쓴다. 클래스 자체, 메서드, 필드, 파라미드드
+
+import { optArg } from "rimraf/dist/commonjs/opt-arg";
+
+
 class Book{
-/**
- * 접근 제한자
- * @public - 누구나
- * @protected - 자식 + 본인인
- * @private - 자기 자신만
- */
-
-    protected title: string;
-
-    printTitle(){
-        console.log(this.title);
-    }
+    private readonly title: string;
 
     constructor(title: string){
-        this.title = title;
+        this.title=title;
+    }
+    
+    @Logging
+    greet(param: string){
+        console.log(this?.title ?? "typescript" + " " + param); 
     }
 
-    somethingImpact(){
-        this.title = "new book!";
+    greet2(param: string){
+        console.log(this.title + "2" + param); 
     }
 }
 
-class Test extends Book{
-    constructor(title: string){
-        super(title);
-    }
+// const book = new Book("abc")
+// book.greet2("이주철")
 
-    printTitleFromChild(): void {
-        console.log(this.title);
-    }
+
+function Logging(
+    originalMethod: Function,
+    context: ClassMethodDecoratorContext
+){
+    return function(this: any, ...args: any[]){
+        console.log("something changed");
+        return originalMethod.call(this, ...args);
+        };
 }
 
-// private readonly 예시
-// class Book{
-//     constructor (
-//         private readonly title: string,
-//         private readonly a: any,
-//         private readonly b: any
-//     ) {}
-// }
-// const b: Book = new Book("inflearn", "hello", "world");
-// //b.title = "hello";
-// console.log(b);
+// 내가 만든 예시시
+function Start(
+    originalMethod: Function,
+    context: ClassMethodDecoratorContext
+){    console.log("hello");
+}
 
 const b: Book = new Book("inflearn");
-//b.title = "hello";
-console.log(b.printTitle());
+b.greet("hello world");
